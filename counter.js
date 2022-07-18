35a746ecf20bfe0c262d832ab94b7e77cdde594c
Rita-cedcoss <ritapal@cedcoss.com>
2022-07-18 14:33:58 +0530

    javascript task 1 js file

---
+++ counter.js
@@ -0,0 +1,16 @@
+
+function decrement()
+{
+    document.getElementById('div21').value-=1;
+
+}
+function increment()
+{
+  let num1=document.getElementById('div21').value;
+    // mum1=parseInt(num1.innerHTML);     
+  num1++;
+  document.getElementById('div21').value=num1;
+ 
+ 
+}
+
