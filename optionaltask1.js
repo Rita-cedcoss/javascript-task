7f84b7feb373962c83362f84168b42a338d09a57
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-11 18:00:46 +0530

    form validation file js

---
+++ optionaltask1.js
@@ -0,0 +1,65 @@
+
+function submit(){
+    // alert()
+var name=document.getElementById('uname').value;
+var email=document.getElementById('uemail').value;
+var passwd=document.getElementById('upasswd').value;
+var valid=true;
+var letters = /^[A-Z][a-z]+\s[A-Z][a-z]+$/;
+var emailCheck=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+var checkpasswd=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+if(!((name.match(letters))))
+    {
+    document.getElementById("message1").innerHTML="Please Enter valid name";
+        valid = false; 
+    }
+    else
+    {
+        document.getElementById("message1").innerHTML = "";
+    }
+   
+    if(!(email.match(emailCheck)))
+    {
+        
+        document.getElementById("message2").innerHTML="Please Enter valid email";
+           valid=false;
+    }
+    else
+    {
+        document.getElementById("message2").innerHTML = "";
+    }
+    
+    if(!(passwd.match(checkpasswd)))
+    {
+        document.getElementById("message3").innerHTML="Please Enter valid password";
+           valid=false;
+    }
+    else
+    {
+        document.getElementById("message3").innerHTML = "";
+    }
+
+    if(valid==true)
+    {
+    
+        alert("Form Submited Successful");
+    document.getElementById('uname').value="";
+    document.getElementById('uemail').value="";
+    document.getElementById('upasswd').value="";
+    // document.getElementById("message1").innerHTML="";
+    // document.getElementById("message2").innerHTML="";
+    // document.getElementById("message3").innerHTML="";
+
+    }
+
+    // else
+    // {
+    //     alert(2);
+    //     // document.getElementById("message2").innerHTML = "fdgfj";
+    // }
+    // if(passwd==""|| passwd<8|| passwd>12)
+    // {
+    //     alert("Please Enter valid password");   
+    // }
+
+}
